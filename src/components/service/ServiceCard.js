8ce720e1819457571e44e7a5c0acf52d77ec5b14
Lucas-Veras <verasluca00@gmail.com>
2022-07-21 17:13:23 -0300

    added services

--- src/components/service/ServiceCard.js
+++ src/components/service/ServiceCard.js
@@ -11,7 +11,9 @@ export default function ServiceCard({ id, name, cost, description, handleRemove 
     return (
         <div className={styles.project_card}>
             <h4>{name}</h4>
-            <p><span>Orçamento: </span>R${cost}</p>
+            <p>
+                <span>Orçamento: </span>R${cost}
+            </p>
             <p>{description}</p>
             <div className={styles.project_card_actions}>
                 <button onClick={remove}>
